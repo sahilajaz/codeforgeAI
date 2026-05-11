bb57ad378c7509d966bfdc2e661e0bf42f687aa1
sahilajaz <sahilajazw@gmail.com>
2026-05-11 14:53:57 +0530

    refactored for cli

--- src/main/java/com/CodeForge/aI/CodeForgeAI/cli/CodeForgeCli.java
+++ src/main/java/com/CodeForge/aI/CodeForgeAI/cli/CodeForgeCli.java
@@ -1,33 +1,20 @@
 package com.CodeForge.aI.CodeForgeAI.cli;
 
-import com.CodeForge.aI.CodeForgeAI.service.AIService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 
 public class CodeForgeCli implements CommandLineRunner {
-    private final AIService aiService;
+    private final CommandRouter commandRouter;
 
-    CodeForgeCli(AIService aiService) {
-        this.aiService = aiService;
+    CodeForgeCli(CommandRouter commandRouter) {
+        this.commandRouter = commandRouter;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
-        if (args.length == 0) {
-            System.out.println("Usage: codeforge \"your prompt\"");
-            return;
-        }
-
-        String message = String.join(" ", args);
-
-        System.out.println("\nCodeForge AI running...\n");
-
-        String response = aiService.askAgent(message);
-
-        System.out.println("\n===== Response =====\n");
-        System.out.println(response);
+        System.out.println(commandRouter.route(args));
     }
 }
